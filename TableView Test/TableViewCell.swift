3243644c16f11bf71e6efbe46875b1dbf9fb99d2
Warren Hansen <whansen1@me.com>
2018-06-12 19:39:51 -0500

    task: added data model

---
+++ TableView Test/TableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  TableViewCell.swift
+//  TableView Test
+//
+//  Created by Warren Hansen on 6/12/18.
+//  Copyright © 2018 Warren Hansen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+    
+
+    @IBOutlet weak var titleTableView: UILabel!
+    @IBOutlet weak var detailTableView: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
--- TableView Test/ViewController.swift
+++ TableView Test/ViewController.swift
@@ -6,8 +6,8 @@
 //  Copyright © 2018 Warren Hansen. All rights reserved.
 //
 
-
-// Mark- TODO - add weather
+// MARK:- TODO - populate from array
+// MARK:- TODO - add weather
 
 import UIKit
 
@@ -15,8 +15,23 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
+    struct Person {
+        var title: String
+        var detail: String
+    }
+    
+    var contacts: [Person] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        contacts.append( Person(title: "Warren Hansen", detail: "Camer Order Silicon Valley") )
+        contacts.append( Person(title: "1 Camera", detail: "Arri Alexa") )
+        contacts.append( Person(title: "1 Primes Zeiss Master Primes", detail: "12mm, 18mm, 21mm, 35mm, 50mm, 85mm, 100mm, 135mm, 150mm, 200mm, ") )
+        contacts.append( Person(title: "1 Primes Zeiss Master Primes", detail: "12mm, 18mm, 21mm, 35mm, 50mm, 85mm, 100mm, 135mm, 150mm, 200mm, ") )
+        contacts.append( Person(title: "1 Primes Zeiss Master Primes", detail: "12mm, 18mm, 21mm, 35mm, 50mm, 85mm, 100mm, 135mm, 150mm, 200mm, ") )
+        contacts.append( Person(title: "1 Primes Zeiss Master Primes", detail: "12mm, 18mm, 21mm, 35mm, 50mm, 85mm, 100mm, 135mm, 150mm, 200mm, ") )
+        contacts.append( Person(title: "1 Primes Zeiss Master Primes", detail: "12mm, 18mm, 21mm, 35mm, 50mm, 85mm, 100mm, 135mm, 150mm, 200mm, ") )
     }
 
     @IBAction func savePDF(_ sender: Any) {
@@ -33,11 +48,13 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 8    
+        return contacts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) //as! FlagCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! ListTableViewCell
+        cell.titleTableView.text = contacts[indexPath.row].title
+        cell.detailTableView.text = contacts[indexPath.row].detail
         return cell
     }
     
